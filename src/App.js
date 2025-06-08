d07c23e6bcf7843008431a75bf9328a400b68035
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-08 22:41:35 +0530

    Add class component with manual this binding (old React style)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React from "react";
-class Conter extends React.Component {
+class Counter extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+  handleDecrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+  handleIncrement() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
   }
   render() {
+    const date = new Date("June 21 2027");
+    date.setDate(date.getDate() + this.state.count);
     return (
       <div>
-        <button>-</button>
-        <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()}
+          {this.state.count}
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
 }
 
-export default Conter;
+export default Counter;
