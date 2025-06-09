934af6b2d4b6ac0485bde2b4a9f138bd47f633a7
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-09 23:52:54 +0530

    Learn about child-to-parent communication in React

--- src/App.js
+++ src/App.js
@@ -70,19 +70,13 @@ class App extends React.Component {
       this.setState({ isLoading: true });
     }
   };
+  setLocation = (e) => this.setState({ location: e.target.value });
 
   render() {
     return (
       <div className="app">
         <h1>CLassy Weather</h1>
-        <div>
-          <input
-            type="test"
-            placeholder="Search from location..."
-            value={this.state.location}
-            onChange={(e) => this.setState({ location: e.target.value })}
-          />
-        </div>
+        <Input location={this.state.location} setLocation={this.setLocation} />
         <button onClick={this.fetchWeather}>Get weather</button>
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
@@ -92,6 +86,21 @@ class App extends React.Component {
             location={this.state.displayLocation}
           />
         )}
+      </div>
+    );
+  }
+}
+
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="test"
+          placeholder="Search from location..."
+          value={this.props.location}
+          onChange={this.props.setLocation}
+        />
       </div>
     );
   }
